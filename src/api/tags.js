bf7170eaa49656ffc32c1e3c9aa47b1cdba6e559
luijp <luijunpeng@outlook.com>
2024-07-11 17:17:51 +0800

    Fix: Tag view

--- src/api/tags.js
+++ src/api/tags.js
@@ -1,13 +1,13 @@
 import request from "./request.js";
 
 export async function getTags(){
-    return (await request.get("/tags/")).data
+    return await request.get("/tags/")
 }
 
 export async function updateTag(tag){
-    return (await request.post('/tags/update', tag)).data
+    return await request.post('/tags/update', tag)
 }
 
 export async function deleteTag(tagId){
-    return (await request.post(`/tags/del/${tagId}`)).data
+    return await request.post(`/tags/del/${tagId}`)
 }
